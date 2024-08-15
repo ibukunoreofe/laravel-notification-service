8866b993451bf8edd0be03248b884e6909d4ecad
Ibukun Bello <ibukunoreofe@gmail.com>
2024-08-15 23:15:44 +0200

    Add telescope

--- app/Events/NotificationSent.php
+++ app/Events/NotificationSent.php
@@ -5,12 +5,12 @@ namespace App\Events;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class NotificationSent implements ShouldBroadcast
 {
-    use InteractsWithSockets;
-    use SerializesModels;
+    use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public $notificationType;
     public $title;
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\NotificationServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
